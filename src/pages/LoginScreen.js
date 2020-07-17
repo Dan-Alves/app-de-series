fcf54fc6e3ce7ee591ee9f66ed2297a0080da5db
Daniel Alves <danielalves.ccomp@gmail.com>
2020-07-17 19:37:44 -0300

    incluindo inputs

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,12 +1,61 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+
+import FormRow from '../components/FormRow';
 
 export default class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mail: '',
+      password: '',
+    }
+  }
+
+  onChangeHandler(field, value) {
+    this.setState({
+      [field]: value
+    });
+  }
+
+  tryLogin() {
+    console.log(this.state);
+  }
+
   render() {
     return (
       <View>
-        <Text>Login!</Text>
+        <FormRow>
+            <TextInput 
+              style={styles.input}
+              placeholder="usuario@email.com"
+              value={this.state.mail}
+              onChangeText={value => this.onChangeHandler('mail', value)}
+            /> 
+        </FormRow>
+        <FormRow>
+            <TextInput 
+              style={styles.input}
+              placeholder="******" 
+              secureTextEntry      
+              value={this.state.password}   
+              onChangeText={value => this.onChangeHandler('password', value)}
+            /> 
+        </FormRow>
+        <Button 
+          title="Entrar" 
+          onPress={() => this.tryLogin()}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom: 5,
+  }
+});
