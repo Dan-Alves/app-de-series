bac7fd525f5f91eb380808c61cb98708ba786150
Daniel Alves <danielalves.ccomp@gmail.com>
2020-07-26 12:26:20 -0300

    Primeiro Login

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import firebase from '@firebase/app';
+import '@firebase/auth';
 
 import FormRow from '../components/FormRow';
 
@@ -11,6 +13,33 @@ export default class LoginPage extends React.Component {
       mail: '',
       password: '',
     }
+  }
+
+  componentDidMount() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyA6CmWLJMnCIrBnNGO_JxkYGwQKZTqIprs",
+      authDomain: "series-539eb.firebaseapp.com",
+      databaseURL: "https://series-539eb.firebaseio.com",
+      projectId: "series-539eb",
+      storageBucket: "series-539eb.appspot.com",
+      messagingSenderId: "78887860483",
+      appId: "1:78887860483:web:9daa98072dfe83a3255136",
+      measurementId: "G-G76PPJPD8D"
+    };
+    // Initialize Firebase
+    if ( ! firebase . apps . length ) {
+      firebase.initializeApp(firebaseConfig);
+    } 
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword('admin@admin.com', 'adminadmin')
+      .then(user => {
+        console.log('Usuário autenticado!', user);
+      })
+      .catch(error => {
+        console.log('Usuário NÃO encontrado', error);
+      })
   }
 
   onChangeHandler(field, value) {
@@ -26,15 +55,6 @@ export default class LoginPage extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <FormRow first>
-            <TextInput 
-              style={styles.input}
-              placeholder="TEMPORARIO"
-              value={this.state.mail}
-              onChangeText={value => this.onChangeHandler('mail', value)}
-            /> 
-        </FormRow>
-
         <FormRow first>
             <TextInput 
               style={styles.input}
